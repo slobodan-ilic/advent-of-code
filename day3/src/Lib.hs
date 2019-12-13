d0d94d8ab7e3f8e5093fab8eb45756886a096cde
Slobodan Ilic <slobodan@crunch.io>
2019-12-14 00:50:11 +0100

    spike: intersections logic

--- day3/src/Lib.hs
+++ day3/src/Lib.hs
@@ -1,6 +1,81 @@
 module Lib
-    ( someFunc
-    ) where
+  ( getNextCoordinate
+  , getIntersection
+  , getCoordinates
+  , getIntersections
+  , getSegments
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+type Coordinate = (Int, Int)
+
+type Coordinates = [Coordinate]
+
+type Instruction = String
+
+type Instructions = [Instruction]
+
+type Segment = (Coordinate, Coordinate)
+
+type Segments = [Segment]
+
+getIntersection :: Segment -> Segment -> Maybe Coordinate
+getIntersection a b =
+  if parallel
+    then Nothing
+    else intersection
+  where
+    ((x1a, y1a), (x2a, y2a)) = a
+    ((x1b, y1b), (x2b, y2b)) = b
+    aTb = (x1a == x2a && y1b == y2b)
+    bTa = (y1a == y2a && x1b == x2b)
+    xaXxb = (x1a > x1b && x1a < x2b || x1a < x1b && x1a > x2b)
+    yaXyb = (y1a > y1b && y1a < y2b || y1a < y1b && y1a > y2b)
+    parallel = (x1a == x2a && x1b == x2b) || (y1a == y2a && y1b == y2b)
+    intersection =
+      if aTb && xaXxb
+        then Just (x1a, y1b)
+        else if bTa && yaXyb
+               then Just (x1b, y1a)
+               else Nothing
+
+getNextCoordinate :: Coordinate -> Instruction -> Coordinate
+getNextCoordinate coord instruction =
+  case direction of
+    'R' -> (x + distance, y)
+    'L' -> (x - distance, y)
+    'U' -> (x, y + distance)
+    'D' -> (x, y - distance)
+  where
+    (x, y) = coord
+    (direction:distStr) = instruction
+    distance = read distStr :: Int
+
+updateCoords :: Coordinates -> Instruction -> Coordinates
+updateCoords coords instruction = coords ++ [next]
+  where
+    next = getNextCoordinate (last coords) instruction
+
+getCoordinates :: Coordinate -> Instructions -> Coordinates
+getCoordinates start instructions = foldl updateCoords [start] instructions
+
+getSegments :: Coordinates -> Segments
+getSegments (_:[]) = []
+getSegments (a:b:xs) = (a, b) : (getSegments (b : xs))
+
+getIntersections :: Segment -> Segments -> Coordinates
+getIntersections a (h:[]) = coordinates
+  where
+    intersection = getIntersection a h
+    Just coordinate = intersection
+    coordinates =
+      if intersection == Nothing
+        then []
+        else coordinate : []
+getIntersections a (h:xs) =
+  if intersection == Nothing
+    then coordinates
+    else coord : coordinates
+  where
+    intersection = getIntersection a h
+    Just coord = intersection
+    coordinates = getIntersections a xs
--- day3/test/Spec.hs
+++ day3/test/Spec.hs
@@ -1,2 +1,60 @@
+--test/Spec.hs
+module Main where
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+  defaultMain $
+    testGroup
+      "Test day3 for advent of code"
+      [ testGetIntersection
+      , testGetCoordinates
+      , testGetSegments
+      , testGetIntersections
+      ]
+
+testGetIntersection :: TestTree
+testGetIntersection =
+  testCase "Testing getting intersection of two segments" $ do
+    assertEqual "Orthogonal but oesn't intersect" Nothing $
+      getIntersection ((2, 0), (3, 0)) ((-1, 1), (1, 1))
+    assertEqual "Parallel doesn't intersect" Nothing $
+      getIntersection ((2, 0), (3, 0)) ((2, 1), (3, 1))
+    assertEqual "Intersects at (0, 0)" (Just (0, 0)) $
+      getIntersection ((1, 0), (-1, 0)) ((0, 1), (0, -1))
+
+testGetCoordinates :: TestTree
+testGetCoordinates =
+  testCase "Test getting coordinates from instructions" $ do
+    assertEqual "Creates list for a single coordinate" [(0, 0), (992, 0)] $
+      getCoordinates (0, 0) ["R992"]
+    assertEqual
+      "Creates list for multiple"
+      [(0, 0), (992, 0), (992, 33), (970, 33), (970, 20)] $
+      getCoordinates (0, 0) ["R992", "U33", "L22", "D13"]
+
+testGetSegments :: TestTree
+testGetSegments =
+  testCase "Test getting segments from coordinates" $ do
+    assertEqual "Creates single segment from two coordinates" [((0, 0), (0, 3))] $
+      getSegments [(0, 0), (0, 3)]
+    assertEqual
+      "Creates two segments from three coordinates"
+      [((0, 0), (0, 3)), ((0, 3), (3, 4))] $
+      getSegments [(0, 0), (0, 3), (3, 4)]
+
+testGetIntersections :: TestTree
+testGetIntersections =
+  testCase "Test getting intersections" $ do
+    assertEqual "Intersects between target and one source" [(1, 0)] $
+      getIntersections ((0, 0), (3, 0)) [((1, -1), (1, 1))]
+    assertEqual
+      "Intersects between target and multiple sources"
+      [(2, 0), (1, 0)] $
+      getIntersections
+        ((0, 0), (3, 0))
+        [((1, -1), (1, -2)), ((2, -1), (2, 3)), ((1, -1), (1, 1))]
