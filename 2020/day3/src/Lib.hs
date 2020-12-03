8866919a819f5f199ebf4f477646ab0a90a266a9
Slobodan Ilic <slobodan@crunch.io>
2020-12-03 20:20:26 +0100

    day3: pt1

--- 2020/day3/src/Lib.hs
+++ 2020/day3/src/Lib.hs
@@ -1,6 +1,21 @@
 module Lib
-    ( someFunc
-    ) where
+  ( nTrees
+  , hit
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+nTrees :: [String] -> Int
+nTrees lines = count 0 lines (1, 3)
+
+count :: Int -> [String] -> (Int, Int) -> Int
+count acc lines (x, y)
+  | x >= length lines = acc
+  | otherwise = count (acc + inc) lines newPos
+  where
+    inc = fromEnum $ hit lines (x, y)
+    newPos = (x + 1, y + 3)
+
+hit :: [String] -> (Int, Int) -> Bool
+hit lines (x, y) = (lines !! i) !! j == '#'
+  where
+    i = x `mod` (length lines)
+    j = y `mod` (length $ lines !! 0)
