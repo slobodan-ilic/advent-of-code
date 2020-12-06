389384d906cf9fd00aa5b6b91f889f3b96a3c694
Slobodan Ilic <slobodan@crunch.io>
2020-12-06 07:14:41 +0100

    day6: pt1

--- 2020/day6/src/Lib.hs
+++ 2020/day6/src/Lib.hs
@@ -1,6 +1,24 @@
 module Lib
-    ( someFunc
-    ) where
+  ( nAnswers
+  , unique
+  , unique'
+  , tokens
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List.Split
+
+nAnswers :: [String] -> Int
+nAnswers lines = sum $ map (length . unique) $ tokens lines
+
+tokens :: [String] -> [String]
+tokens lines = map concat $ splitOn [""] lines
+
+unique :: String -> String
+unique x = unique' [] x
+
+unique' :: String -> String -> String
+unique' acc [] = acc
+unique' acc (x:xs) =
+  case elem x acc of
+    True -> unique' acc xs
+    _ -> unique' (x : acc) xs
