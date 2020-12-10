73791f11b69b468452516aececb694819cb0addf
Slobodan Ilic <slobodan@crunch.io>
2020-12-10 09:40:46 +0100

    day10: pt1&2

--- 2020/day10/app/Main.hs
+++ 2020/day10/app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
+import Data.List
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  contents <- readFile "input.txt"
+  putStrLn $
+    show $
+    countPaths $ (0 : (sort $ map (\line -> read line :: Int) (lines contents)))
--- 2020/day10/src/Lib.hs
+++ 2020/day10/src/Lib.hs
@@ -1,6 +1,31 @@
 module Lib
-    ( someFunc
-    ) where
+  ( countPaths
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List.Split
+
+type Adapter = Int
+
+type Adapters = [Adapter]
+
+type Difference = Int
+
+type Segment = [Difference]
+
+countPaths :: Adapters -> Int
+countPaths adapters = product $ map nPaths segments
+  where
+    diffs = diffSegment adapters
+    segments = splitOn [3] diffs
+
+nPaths :: Segment -> Int
+nPaths segment =
+  case length segment of
+    4 -> 7
+    3 -> 4
+    2 -> 2
+    _ -> 1
+
+diffSegment :: Adapters -> Segment
+diffSegment (a:[]) = []
+diffSegment (a:b:as) = (b - a) : (diffSegment (b : as))
