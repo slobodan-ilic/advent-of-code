a72782ccb02128085df564e54c3cbc836a4cb3a5
Slobodan Ilic <slobodan@crunch.io>
2020-12-13 16:20:54 +0100

    day13: pt1 & 2

--- 2020/day13/app/Main.hs
+++ 2020/day13/app/Main.hs
@@ -3,4 +3,7 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  contents <- readFile "input.txt"
+  putStrLn $ show $ runExample contents
+  -- putStrLn $ show $ readInput contents
--- 2020/day13/src/Lib.hs
+++ 2020/day13/src/Lib.hs
@@ -1,6 +1,53 @@
 module Lib
-    ( someFunc
-    ) where
+  ( readInput
+  , runExample
+  , findTime
+  , findTime'
+  , findNext
+  , Bus
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List
+import Data.List.Split
+
+type BusId = Int
+
+type Pos = Int
+
+data Bus =
+  Bus BusId Pos
+  deriving (Show)
+
+type Busses = [Bus]
+
+type Time = Int
+
+runExample :: String -> Time
+runExample contents = findTime busses
+  where
+    busses = readInput contents
+
+readInput :: String -> Busses
+readInput contents =
+  [Bus (read id :: Int) pos | (id, pos) <- zip strIds [0 ..], id /= "x"]
+  where
+    lns = lines contents
+    strIds = splitOn "," $ lns !! 1
+
+findTime :: Busses -> Time
+findTime busses = findTime' 0 1 busses
+
+findTime' :: Time -> Int -> Busses -> Time
+findTime' time _ [] = time
+findTime' time mult (bus@(Bus id pos):busses) = findTime' time' mult' busses
+  where
+    (time', mult') = findNext time mult bus
+
+findNext :: Time -> Int -> Bus -> (Time, Int)
+findNext time mult bus@(Bus id pos) =
+  if timeRem == busRem
+    then (time, mult * id)
+    else findNext (time + mult) mult bus
+  where
+    busRem = (-pos) `mod` id
+    timeRem = time `mod` id
