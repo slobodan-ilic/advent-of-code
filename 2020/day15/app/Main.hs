4c97edc8efec7abefe92f73c0c369db09fbe01f0
Slobodan Ilic <slobodan@crunch.io>
2020-12-15 14:28:08 +0100

    day15: pt1 & 2

--- 2020/day15/app/Main.hs
+++ 2020/day15/app/Main.hs
@@ -3,4 +3,6 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  contents <- readFile "input.txt"
+  putStrLn $ show $ generateList 30000000 (parseInput contents)
--- 2020/day15/src/Lib.hs
+++ 2020/day15/src/Lib.hs
@@ -1,6 +1,32 @@
 module Lib
-    ( someFunc
-    ) where
+  ( parseInput
+  , generateList
+  ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.IntMap.Strict as Map
+import Data.List
+import Data.List.Split
+
+type Memory = Map.IntMap Int
+
+parseInput :: String -> [Int]
+parseInput str = map (\el -> read el :: Int) nums
+  where
+    nums = splitOn "," str
+
+generateList :: Int -> [Int] -> Int
+generateList nMax starting =
+  generateList' initMap initCounter nMax (last starting)
+  where
+    initMap =
+      Map.fromList $ init [(key, val) | (key, val) <- zip starting [0 ..]]
+    initCounter = length starting
+
+generateList' :: Memory -> Int -> Int -> Int -> Int
+generateList' memory counter nMax current
+  | counter >= nMax = current
+  | otherwise =
+    generateList' (Map.insert current def memory) (counter + 1) nMax next
+  where
+    def = counter - 1
+    next = def - (Map.findWithDefault def current memory)
