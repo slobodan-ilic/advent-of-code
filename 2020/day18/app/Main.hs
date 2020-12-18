b1e8f638e0245b62e0265a4c2286c06283d1777b
Slobodan Ilic <slobodan@crunch.io>
2020-12-18 07:15:09 +0100

    day18: pt1 & 2

--- 2020/day18/app/Main.hs
+++ 2020/day18/app/Main.hs
@@ -1,6 +1,19 @@
 module Main where
 
+import Control.Monad
+import Language.Haskell.Interpreter
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+  contents <- readFile "input.txt"
+  r <-
+    runInterpreter $ do
+      loadModules ["src/Lib.hs"]
+      setTopLevelModules ["Lib"]
+      setImports ["Prelude"]
+      a <- eval $ transformExpr contents
+      liftIO $ print a
+  case r of
+    Left err -> print err
+    Right () -> return ()
--- 2020/day18/src/Lib.hs
+++ 2020/day18/src/Lib.hs
@@ -1,6 +1,15 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List
+import Data.List.Split
+
+plus :: Int -> Int -> Int
+plus a b = a + b
+
+mul :: Int -> Int -> Int
+mul a b = a * b
+
+transformExpr :: String -> String
+transformExpr contents = intercalate "`plus`" $ splitOn "+" line
+  where
+    line = "(" ++ (intercalate ") + (" (lines contents)) ++ ")"
