3849a1236b7457edca6b3bc0ac54e986e08655de
Slobodan Ilic <slobodan@crunch.io>
2020-12-22 07:18:59 +0100

    day19: pt1 & 2
    * optimize this code

--- 2020/day19/app/Main.hs
+++ 2020/day19/app/Main.hs
@@ -1,6 +1,32 @@
-module Main where
+import Data.List.Split
+import Data.Map (Map, (!))
+import qualified Data.Map as M
 
-import Lib
+data Rule
+  = Const Char
+  | Rule [[Int]]
+  deriving (Show)
 
-main :: IO ()
-main = someFunc
+parse :: String -> (Map Int Rule, [String])
+parse input =
+  let [rules, inp] = splitOn "\n\n" input
+   in (M.fromList $ map f $ lines rules, lines inp)
+  where
+    f x =
+      let [k, v] = splitOn ": " x
+       in if head v == '"'
+            then (read k, Const (v !! 1))
+            else (read k, Rule $ map (map read . words) $ splitOn " | " v)
+
+matches :: Map Int Rule -> String -> Bool
+matches rules = any null . (go (rules M.! 0))
+  where
+    rule0 = rules M.! 0
+    go :: Rule -> String -> [String]
+    go _ [] = []
+    go (Const c) (x:xs) =
+      if c == x
+        then [xs]
+        else []
+    go (Rule rs) xs =
+      concatMap (foldl (\ys r -> concatMap (go r) ys) [xs] . map (rules !)) rs
--- 2020/day19/src/Lib.hs
+++ 2020/day19/src/Lib.hs
@@ -1,6 +1,52 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List.Split
+import qualified Data.Map as M
+
+type Seg = [Int]
+
+type Segs = [Seg]
+
+data Rule
+  = Leaf Char
+  | Node [Seg]
+  deriving (Show)
+
+type Rules = M.Map Int Rule
+
+process :: [String] -> [String] -> [String]
+process cs pics = filter (\pic -> pic `elem` cs) pics
+
+combs :: Rules -> Seg -> [String]
+combs rs [] = [""]
+combs rs seg@(id:ids) =
+  case rule of
+    Leaf c -> [c : r | r <- rest]
+    Node segs ->
+      [s ++ r | r <- rest, s <- concat [combs rs seg' | seg' <- segs]]
+  where
+    rule = M.findWithDefault (Leaf 'X') id rs
+    rest = combs rs ids
+
+-- parse :: String -> (Rules, [String])
+-- parse contents = (rules, pts !! 1)
+--   where
+--     lns = lines contents
+--     pts = splitOn [""] lns
+--     rules = M.fromList $ map parseRule (pts !! 0)
+parseRule :: String -> (Int, Rule)
+parseRule str = (id, rule)
+  where
+    id = read (takeWhile (/= ':') str) :: Int
+    rest = drop 2 $ dropWhile (/= ':') str
+    ptsAny = map (dropWhile (== ' ')) (splitOn "|" rest)
+    rule =
+      case '"' `elem` rest of
+        True -> Leaf (rest !! 1)
+        False -> Node $ map parseSeg ptsAny
+
+parseSeg :: String -> Seg
+parseSeg str = nums
+  where
+    segs = filter (/= "") $ splitOn " " str
+    nums = map (\s -> read s :: Int) segs
